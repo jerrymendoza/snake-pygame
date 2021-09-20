821217a7f9507e7d198297e5ba6ab150925580ec
Jerry Mendoza <jamendoza@uc.cl>
2021-09-20 11:15:55 -0300

    refactor(player): key is pressed

--- Snake Game.py
+++ Snake Game.py
@@ -64,37 +64,19 @@ def show_score(choice, color, font, size):
 # Main logic
 while True:
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         # Whenever a key is pressed down
         elif event.type == pygame.KEYDOWN:
-            # W -> Up; S -> Down; A -> Left; D -> Right
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                change_to = 'UP'
-            if event.key == pygame.K_DOWN or event.key == ord('s'):
-                change_to = 'DOWN'
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                change_to = 'LEFT'
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                change_to = 'RIGHT'
-            # Esc -> Create event to quit the game
-            if event.key == pygame.K_ESCAPE:
-                pygame.event.post(pygame.event.Event(pygame.QUIT))
+           snake.set_change(event)
 
     # Making sure the snake cannot move in the opposite direction instantaneously
-    if change_to == 'UP' and direction != 'DOWN':
-        direction = 'UP'
-    if change_to == 'DOWN' and direction != 'UP':
-        direction = 'DOWN'
-    if change_to == 'LEFT' and direction != 'RIGHT':
-        direction = 'LEFT'
-    if change_to == 'RIGHT' and direction != 'LEFT':
-        direction = 'RIGHT'
-
+    snake.set_direction()
 
     # Moving the snake and grow
-    score = snake.update(direction, food, score)
+    score = snake.update(food, score)
 
     # Spawning food on the screen
     if not food.active:
--- elements/player.py
+++ elements/player.py
@@ -5,9 +5,11 @@ class Snake(Element):
     def __init__(self):
         super().__init__(100, 50)
         self.body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
+        self.direction = 'RIGHT'
+        self.change_to = self.direction
 
-    def update(self, direction, food, score):
-        self._move(direction)
+    def update(self, food, score):
+        self._move(self.direction)
         return self._grow(food, score)
 
         
@@ -29,3 +31,32 @@ class Snake(Element):
         else:
             self.body.pop()
         return score
+
+    def body_collision(self):
+        for block in self.body[1:]:
+            return self.x == block[0] and self.y == block[1]
+
+    def set_change(self, event):
+
+        # W -> Up; S -> Down; A -> Left; D -> Right
+        if event.key == pygame.K_UP or event.key == ord('w'):
+            self.change_to = 'UP'
+        if event.key == pygame.K_DOWN or event.key == ord('s'):
+            self.change_to = 'DOWN'
+        if event.key == pygame.K_LEFT or event.key == ord('a'):
+            self.change_to = 'LEFT'
+        if event.key == pygame.K_RIGHT or event.key == ord('d'):
+            self.change_to = 'RIGHT'
+        # Esc -> Create event to quit the game
+        if event.key == pygame.K_ESCAPE:
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
+        
+    def set_direction(self):
+        if self.change_to == 'UP' and self.direction != 'DOWN':
+            self.direction = 'UP'
+        if self.change_to == 'DOWN' and self.direction != 'UP':
+            self.direction = 'DOWN'
+        if self.change_to == 'LEFT' and self.direction != 'RIGHT':
+            self.direction = 'LEFT'
+        if self.change_to == 'RIGHT' and self.direction != 'LEFT':
+            self.direction = 'RIGHT'
